ddc65de0f5a6031ed96b925cab289517e2671b44
Alima07 <alex.lima_dev@outlook.com>
2022-07-08 16:06:15 -0300

    Implementação da função buscar e correção da chamada das funções dentro da variavel funcoes como objeto, cada função

--- funcoes.js
+++ funcoes.js
@@ -1,15 +1,31 @@
 const cachorros = require('./database/cachorros.json');
 const fs = require('fs');
+const { findSourceMap } = require('module');
 
 function salvar(){
 
     fs.writeFileSync(__dirname + '/database/cachorros.json',JSON.stringify(cachorros, null, 4));
 }
 
-function buscar(id){}
+function buscar(id){
+
+    let cachorrosId = cachorros.find(cachorros => cachorros.id == id);
+    cachorrosId == undefined ? console.log("Erro: ID CACHORRO NÃO ENCONTRADO") : console.log(cachorrosId);
+}
+
+
 
 let funcoes = {
 
+    listar: () => {
+
+
+    },
+    
+    descrever: () => {
+        
+
+    },
     
 }
 
